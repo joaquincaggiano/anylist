9a124ff44a448a3e65ac9a82dd7277ad9d45a539
Moilab <moilab.web@gmail.com>
2026-05-25 13:16:50 +0200

    feat(): remove item

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -43,7 +43,10 @@ export class ItemsService {
     return await this.itemRepository.save(item);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} item`;
+  async remove(id: string): Promise<Item> {
+    const item = await this.findOne(id);
+    await this.itemRepository.remove(item);
+
+    return { ...item, id };
   }
 }

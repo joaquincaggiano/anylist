bd6ccb5bcbf4741b64468a76ebe79ba3b5e44a86
Moilab <moilab.web@gmail.com>
2026-05-27 11:24:09 +0200

    feat(): validate findAll with auhtentitcated user

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,22 +1,31 @@
-import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { UpdateUserInput } from './dto/inputs/update-user.input';
-import { ParseUUIDPipe } from '@nestjs/common';
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { ValidRolesArgs } from './dto/args/roles.args';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  async findAll(@Args() validRoles: ValidRolesArgs): Promise<User[]> {
+  async findAll(
+    @Args() validRoles: ValidRolesArgs,
+    @CurrentUser([ValidRoles.ADMIN]) user: User,
+  ): Promise<User[]> {
     return this.usersService.findAll(validRoles.roles);
   }
 
   @Query(() => User, { name: 'findUserById' })
   async findOneById(
     @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser([ValidRoles.ADMIN, ValidRoles.USER, ValidRoles.SUPER_USER])
+    user: User,
   ): Promise<User> {
     return this.usersService.findOneById(id);
   }

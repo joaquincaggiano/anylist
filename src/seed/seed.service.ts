082d262561ba67ffb3c446c668b2e8a234ffd896
Moilab <moilab.web@gmail.com>
2026-05-28 11:03:32 +0200

    feat(): seed protected

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeedService {
-  constructor() {}
+  private isProd: boolean;
+  constructor(private readonly configService: ConfigService) {
+    this.isProd = configService.get('STATE') === 'prod';
+  }
 
   async executeSeed(): Promise<boolean> {
+    if (this.isProd) {
+      throw new BadRequestException('We cannot run seed on production');
+    }
     return true;
   }
 }

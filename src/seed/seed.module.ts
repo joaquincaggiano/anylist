aa0b35788f3d7af30313af6aeb5f08d00fe60592
Moilab <moilab.web@gmail.com>
2026-05-28 11:39:05 +0200

    feat(): delete items and users in seed

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedResolver } from './seed.resolver';
 import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from 'src/items/items.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, ItemsModule, UsersModule],
   providers: [SeedResolver, SeedService],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,20 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/items/entities/item.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {
     this.isProd = configService.get('STATE') === 'prod';
   }
 
@@ -12,6 +22,14 @@ export class SeedService {
     if (this.isProd) {
       throw new BadRequestException('We cannot run seed on production');
     }
+    // Purgamos la base de datos
+    await this.deleteDatabase();
+
     return true;
+  }
+
+  async deleteDatabase() {
+    await this.itemsRepository.createQueryBuilder().delete().execute();
+    await this.usersRepository.createQueryBuilder().delete().execute();
   }
 }

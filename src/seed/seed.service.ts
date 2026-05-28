04655bcaa3d07bbb7c734a003e58ba4d81c86a62
Moilab <moilab.web@gmail.com>
2026-05-28 11:51:28 +0200

    feat(): seed data and load users

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,6 +4,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Item } from 'src/items/entities/item.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { SEED_USERS } from './data/seed-data';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class SeedService {
@@ -14,6 +17,7 @@ export class SeedService {
     private readonly itemsRepository: Repository<Item>,
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    private readonly usersService: UsersService,
   ) {
     this.isProd = configService.get('STATE') === 'prod';
   }
@@ -25,11 +29,24 @@ export class SeedService {
     // Purgamos la base de datos
     await this.deleteDatabase();
 
+    // Cargamos los usuarios
+    const user = await this.loadUsers();
+
     return true;
   }
 
   async deleteDatabase() {
     await this.itemsRepository.createQueryBuilder().delete().execute();
     await this.usersRepository.createQueryBuilder().delete().execute();
+  }
+
+  async loadUsers(): Promise<User> {
+    const users: User[] = [];
+
+    for (const user of SEED_USERS) {
+      users.push(await this.usersService.create(user));
+    }
+
+    return users[0];
   }
 }
